c1814af7c79fccdb3a5885ba50a4afb47b8d7d82
Marina Charaeva <marina_charaeva@yahoo.com>
2026-07-26 14:27:56 +0000

    fix errors

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # Устанавливаем зависимости
-uv sync
-
+uv pip install --upgrade pip
+uv pip install django-bootstrap5 django django-environ dj-database-url gunicorn psycopg2-binary whitenoise
 # Собираем статику
 uv run python manage.py collectstatic --noinput
 
