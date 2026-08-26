da0da303d08813170ef09be6c842b64bb6beb167
Marina Charaeva <marina_charaeva@yahoo.com>
2026-08-26 17:42:02 +0000

    fix

---
+++ task_manager/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+class Status(models.Model):
+    """Модель статуса задачи"""
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_('Name')
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
+    
+    class Meta:
+        verbose_name = _('Status')
+        verbose_name_plural = _('Statuses')
+        ordering = ['id']
+    
+    def __str__(self):
+        return self.name
